790ae2adfbadd6d483a781e0c510ca3eae1c99dc
Tamara Salvatori <Tamara.Salvatori-ext@ab-inbev.com>
2021-11-29 21:11:32 -0300

    Change location API Key

--- finalMovieApp/API/API.swift
+++ finalMovieApp/API/API.swift
@@ -5,25 +5,28 @@ import Alamofire
 
 private let baseUrl = "https://api.themoviedb.org/3/movie/"
 let posterUrl = "https://image.tmdb.org/t/p/original"
-//let idMovie = "https://api.themoviedb.org/3/movie/550?api_key=396aa785169ed778a7132e7a8dedc31f"
 private let coder = JSONDecoder()
-private let apikey = "396aa785169ed778a7132e7a8dedc31f"
 
 class API {
 
     class func fetchMovies(_ page: Int, onSuccess: @escaping (Results) -> Void) {
         coder.keyDecodingStrategy = .convertFromSnakeCase
-        let urlStr = "\(baseUrl)top_rated?api_key=\(apikey)&page=\(page)"
+        let urlStr = "\(baseUrl)top_rated?api_key=\(Configuration.apiKey)&page=\(page)"
         guard let url = URL(string: urlStr) else { fatalError("Unable to get url") }
 
         AF.request(url).response { response in
             switch response.result {
             case .success(let data):
                 guard let data = data else { fatalError("Unable to parse data from API") }
-                guard let results = try? coder.decode(Results.self, from: data) else { fatalError("Unable to parse data in json") }
-                DispatchQueue.main.async {
-                    onSuccess(results)
+                do {
+                    let results = try coder.decode(Results.self, from: data)
+                    DispatchQueue.main.async {
+                        onSuccess(results)
+                    }
                 }
+                    catch {
+                        print(error)
+                    }
             case .failure(let error):
                 print(error.localizedDescription)
             }
@@ -32,7 +35,7 @@ class API {
 
     class func fetchMoviesId(_ id: Int, onSuccess: @escaping (MovieDetails) -> Void) {
         coder.keyDecodingStrategy = .convertFromSnakeCase
-        let urlStr = "\(baseUrl)\(String(describing: id))?api_key=\(apikey)"
+        let urlStr = "\(baseUrl)\(String(describing: id))?api_key=\(Configuration.apiKey)"
         guard let url = URL(string: urlStr) else { fatalError("Unable to get url") }
 
         AF.request(url).response { response in
